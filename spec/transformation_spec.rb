823509caafe8b7ffd693567ddb8c99136fba626c
Francesco Lazzarino <flazzarino@gmail.com>
2010-01-27 14:09:54 -0500

    properly redirecting

--- spec/transformation_spec.rb
+++ spec/transformation_spec.rb
@@ -143,11 +143,54 @@ end
 
 describe "/migration" do
   before(:all) { @url = "/migration" }
+
   it_should_behave_like "any transformation"
+
+  it "should redirect to the proper transformation" do
+    pending "no migrations defined in any actionplan yet"
+  end
+
 end
 
 describe "/normalization" do
   before(:all) { @url = "/normalization" }
+
   it_should_behave_like "any transformation"
+
+  it "should redirect to the proper transformation" do
+    premis_object = <<-XML
+<?xml version="1.0" encoding="UTF-8"?>
+<object xmlns="info:lc/xmlns/premis-v2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="file">
+  <objectIdentifier>
+    <objectIdentifierType>URI</objectIdentifierType>
+    <objectIdentifierValue>sha1:7ac8b064b38fe4d42f1b04ea45e43f71</objectIdentifierValue>
+  </objectIdentifier>
+  <objectCharacteristics>
+    <compositionLevel>0</compositionLevel>
+    <fixity>
+      <messageDigestAlgorithm>MD5</messageDigestAlgorithm>
+      <messageDigest>7ac8b064b38fe4d42f1b04ea45e43f71</messageDigest>
+    </fixity>
+    <size>5138268</size>
+    <format>
+      <formatDesignation>
+        <formatName>WAVE</formatName>
+      </formatDesignation>
+    </format>
+    <objectCharacteristicsExtension>
+      <audioObject xmlns="http://www.aes.org/audioObject">
+        <audioDataEncoding>PCM</audioDataEncoding>
+      </audioObject>
+    </objectCharacteristicsExtension>
+
+  </objectCharacteristics>
+  <originalName>mimi.pdf</originalName>
+</object>
+    XML
+
+    post @url, :description => premis_object
+    last_response.status.should == 302
+    last_response['Location'].should == 'http://localhost:7000/transformation/transform/wave_norm'
+  end
 
 end
